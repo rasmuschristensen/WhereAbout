01ae19161608b65d60fbbf57445f146d74846823
Rasmus Tolstrup Christensen <rasmus.christensen@gmail.com>
2013-04-15 21:08:22 +0200

    added images and corrected path to scripts and styles

---
+++ src/Client/javascripts/views/mainpage.js
@@ -0,0 +1,38 @@
+﻿
+var t;
+function onPullDown(event, data) {
+
+    setTimeout(function gotPullDownData() {
+        data.iscrollview.refresh();
+    }, 1500);
+}
+    
+
+$(document).on("pageinit", "#mainPage", function () {
+
+       
+
+    /* Open settings or tasklist panel depending on the swipe direction */
+    $(document).on("swipeleft swiperight", "#mainPage", function (e) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ($.mobile.activePage.jqmData("panel") !== "open") {
+            if (e.type === "swipeleft") {
+                $("#task-panel").panel("open");
+            } else if (e.type === "swiperight") {
+                $("#setting-panel").panel("open");
+            }
+        }
+    });
+
+    /* Close the task list panel when a task i clicked */
+    $(document).on("click", "#task-list", function (e) {
+        $("#task-panel").panel("close");
+    });
+
+    /* When the tasklist is pulled down we add a hook to refresh its data */
+    $(document).on("iscroll_onpulldown", ".iscroll-wrapper", function (event, data) {
+        onPullDown(event, data);
+    });
+});
